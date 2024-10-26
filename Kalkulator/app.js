a180987ecbba2606a79eed9d7d18e18c3622c026
Chocolaatee <firokusuma123@gmail.com>
2024-10-27 01:30:30 +0700

    initialization commit

---
+++ Kalkulator/app.js
@@ -0,0 +1,22 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.innerText += value;
+}
+
+function clearDisplay() {
+    display.innerText = '';
+}
+
+function deleteLast() {
+    display.innerText = display.innerText.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.innerText = eval(display.innerText);
+    } catch (error) {
+        display.innerText = 'Error';
+    }
+}
+
